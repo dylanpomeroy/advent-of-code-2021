4a95cb85378f770dbf8fcc91005e132f4787e216
Dylan Pomeroy <dylan@dylanpomeroy.com>
2021-12-05 02:03:48 -0500

    refactor q5b

--- src/question5/question.ts
+++ src/question5/question.ts
@@ -1,132 +1,94 @@
-const partA = async (input: string[]) => {
+const createMap = (size: number) => {
   const map: number[][] = []
-  for (let i = 0; i < 1000; i++) {
-    const row = []
-    for (let j = 0; j < 1000; j++) {
-      row.push(0)
-    }
-    map.push(row)
+  for (let rowIndex = 0; rowIndex < size; rowIndex++) {
+    map.push(Array(size).fill(0))
   }
+
+  return map
+}
+
+const getPointsFromInputLine = (inputLine: string): { pointA: number[], pointB: number[] } => {
+  const pointStrings = inputLine.split(' -> ')
+    .map(pointString => pointString.split(',')
+      .map(numString => parseInt(numString)))
+  
+  return {
+    pointA: pointStrings[0],
+    pointB: pointStrings[1],
+  }
+}
+
+const countGreaterThanIn = (map: number[][], greaterThan: number): number => {
+  let countOfGreaterThan = 0
+  for (let i = 0; i < map.length; i++)
+    for (let j = 0; j < map[0].length; j++)
+      if (map[i][j] > greaterThan) countOfGreaterThan++
+  return countOfGreaterThan
+}
+
+const partA = async (input: string[]) => {
+  const map = createMap(1000)
 
   for (let lineInput of input) {
-    const point0 = lineInput.split(' -> ')[0].split(',').map(numString => parseInt(numString))
-    const point1 = lineInput.split(' -> ')[1].split(',').map(numString => parseInt(numString))
+    const { pointA, pointB } = getPointsFromInputLine(lineInput)
 
-    if (point0[0] == point1[0]) {
+    if (pointA[0] == pointB[0])
       // vertical line
-      if (point0[1] < point1[1]) {
+      if (pointA[1] < pointB[1])
         // point0 is lower than point1
-        for (let i = point0[1]; i <= point1[1]; i++) {
-          map[i][point0[0]] += 1
-        }
-      } else {
+        for (let i = pointA[1]; i <= pointB[1]; i++)
+          map[i][pointA[0]] += 1
+      else
         // point1 is lower than point0
-        for (let i = point1[1]; i <= point0[1]; i++) {
-          map[i][point0[0]] += 1
-        }
-      }
-    } else if (point0[1] == point1[1]) {
+        for (let i = pointB[1]; i <= pointA[1]; i++)
+          map[i][pointA[0]] += 1
+    else if (pointA[1] == pointB[1])
       // horizontal line
-      if (point0[0] < point1[0]) {
+      if (pointA[0] < pointB[0])
         // point0 is left of point1
-        for (let i = point0[0]; i <= point1[0]; i++) {
-          map[point0[1]][i] += 1
-        }
-      } else {
+        for (let i = pointA[0]; i <= pointB[0]; i++)
+          map[pointA[1]][i] += 1
+      else
         // point1 is right of point0
-        for (let i = point1[0]; i <= point0[0]; i++) {
-          map[point0[1]][i] += 1
-        }
-      }
-    }
+        for (let i = pointB[0]; i <= pointA[0]; i++)
+          map[pointA[1]][i] += 1
   }
 
-  let countOfGreaterThan2 = 0
-  for (let i = 0; i < 1000; i++) {
-    for (let j = 0; j < 1000; j++) {
-      if (map[i][j] > 1) countOfGreaterThan2++
-    }
-  }
-
-  return countOfGreaterThan2
+  return countGreaterThanIn(map, 1);
 }
 
 const partB = async (input: string[]) => {
-  const mapSize = 1000
-  const map: number[][] = []
-  for (let i = 0; i < mapSize; i++) {
-    const row = []
-    for (let j = 0; j < mapSize; j++) {
-      row.push(0)
-    }
-    map.push(row)
-  }
+
+  const map = createMap(1000)
 
   for (let lineInput of input) {
-    const point0 = lineInput.split(' -> ')[0].split(',').map(numString => parseInt(numString))
-    const point1 = lineInput.split(' -> ')[1].split(',').map(numString => parseInt(numString))
+    const { pointA, pointB } = getPointsFromInputLine(lineInput)
+    const diagnonalLength = Math.abs(pointA[0] - pointB[0])
 
-    if (point0[0] == point1[0]) {
-      // vertical line
-      if (point0[1] < point1[1]) {
-        // point0 is lower than point1
-        for (let i = point0[1]; i <= point1[1]; i++) {
-          map[i][point0[0]] += 1
-        }
-      } else {
-        // point1 is lower than point0
-        for (let i = point1[1]; i <= point0[1]; i++) {
-          map[i][point0[0]] += 1
-        }
-      }
-    } else if (point0[1] == point1[1]) {
-      // horizontal line
-      if (point0[0] < point1[0]) {
-        // point0 is left of point1
-        for (let i = point0[0]; i <= point1[0]; i++) {
-          map[point0[1]][i] += 1
-        }
-      } else {
-        // point1 is right of point0
-        for (let i = point1[0]; i <= point0[0]; i++) {
-          map[point0[1]][i] += 1
-        }
-      }
-    } else if (point0[0] < point1[0] && point0[1] < point1[1]) {
-      // top left point0 to bottom right point1
-      const length = Math.abs(point0[0] - point1[0])
-      for (let i = 0; i <= length; i++) {
-        map[point0[1] + i][point0[0] + i] += 1
-      }
-    } else if (point0[0] < point1[0] && point0[1] > point1[1] ) {
-      // bottom left point0 to top right point1
-      const length = Math.abs(point0[0] - point1[0])
-      for (let i = 0; i <= length; i++) {
-        map[point0[1] - i][point0[0] + i] += 1
-      }
-    } else if (point0[0] > point1[0] && point0[1] < point1[1]) {
-      // top right point0 to bottom left point1
-      const length = Math.abs(point0[0] - point1[0])
-      for (let i = 0; i <= length; i++) {
-        map[point0[1] + i][point0[0] - i] += 1
-      }
-    } else if (point0[0] > point1[0] && point0[1] > point1[1]) {
-      // bottom right point0 to top left point1
-      const length = Math.abs(point0[0] - point1[0])
-      for (let i = 0; i <= length; i++) {
-        map[point0[1] - i][point0[0] - i] += 1
+    const isVerticalLeftToRight = pointA[0] == pointB[0] && pointA[1] < pointB[1]
+    const isVerticalRightToLeft = pointA[0] == pointB[0] && pointA[1] >= pointB[1]
+    const isHorizontalLeftToRight = pointA[1] == pointB[1] && pointA[0] < pointB[0]
+    const isHorizontalRightToLeft = pointA[1] == pointB[1] && pointA[0] >= pointB[0]
+    const isDiagonalTopLeftToBottomRight = pointA[0] < pointB[0] && pointA[1] < pointB[1]
+    const isDiagonalBottomLeftToTopRight = pointA[0] < pointB[0] && pointA[1] > pointB[1]
+    const isDiagonalTopRightToBottomLeft = pointA[0] > pointB[0] && pointA[1] < pointB[1]
+    const isDiagonalBottomRightToTopLeft = pointA[0] > pointB[0] && pointA[1] > pointB[1]
+
+    if (isVerticalLeftToRight) for (let i = pointA[1]; i <= pointB[1]; i++) map[i][pointA[0]] += 1
+    else if (isVerticalRightToLeft) for (let i = pointB[1]; i <= pointA[1]; i++) map[i][pointA[0]] += 1 
+    else if (isHorizontalLeftToRight) for (let i = pointA[0]; i <= pointB[0]; i++) map[pointA[1]][i] += 1
+    else if (isHorizontalRightToLeft) for (let i = pointB[0]; i <= pointA[0]; i++) map[pointA[1]][i] += 1
+    else {
+      for (let i = 0; i <= diagnonalLength; i++) {
+        if (isDiagonalTopLeftToBottomRight) map[pointA[1] + i][pointA[0] + i] += 1
+        else if (isDiagonalBottomLeftToTopRight) map[pointA[1] - i][pointA[0] + i] += 1
+        else if (isDiagonalTopRightToBottomLeft) map[pointA[1] + i][pointA[0] - i] += 1
+        else if (isDiagonalBottomRightToTopLeft) map[pointA[1] - i][pointA[0] - i] += 1
       }
     }
   } 
 
-  let countOfGreaterThan2 = 0
-  for (let i = 0; i < mapSize; i++) {
-    for (let j = 0; j < mapSize; j++) {
-      if (map[i][j] > 1) countOfGreaterThan2++
-    }
-  }
-
-  return countOfGreaterThan2
+  return countGreaterThanIn(map, 1);
 }
 
 export default {
